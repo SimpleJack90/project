cda0aef9436fca0a19bb31dad7c84d1fc3a11982
SimpleJack90 <nemanja90petrovic@hotmail.com>
2019-06-18 14:16:26 +0200

    Added User class and method to login user.

--- main/login.php
+++ main/login.php
@@ -46,7 +46,7 @@
                 <div class="col-md-3"></div>
                 <div class="col-md-6 login-form-1">
                     <h3>Login</h3>
-                    <form>
+                    <form method="POST" action="login.php">
                         <div class="form-group">
                             <input type="text" class="form-control" placeholder="Your Email *" value="" />
                         </div>
---
+++ objects/users.php
@@ -0,0 +1,35 @@
+<?php
+
+class Users{
+
+    private $conn;
+    private $table_name="users";
+
+    public $id;
+    public $user_name;
+    public $email;
+    public $password;
+
+    public function __construct($db_connection){
+        $this->conn=$db_connection;
+    }
+
+    public function CheckIfLogged($email,$password){
+        
+        $query="SELECT * 
+                FROM ".$this->table_name." 
+                WHERE email=:email AND password=:password";
+        
+        $stmt=$this->conn->prepare($query);
+        
+        $stmt.bindParam(':email',$email);
+        $stmt.bindParam(':password',$password);
+
+        $stmt->execute();
+
+        return $stmt;
+
+    }
+}
+
+?>
