afc2195659b64e06aac4f27661c0c3d1b437112b
SimpleJack90 <nemanja90petrovic@hotmail.com>
2019-06-21 12:06:31 +0200

    From for create articles successfully implemented

---
+++ assets/js/upload_article_imgs.js
@@ -0,0 +1,169 @@
+// if document ready do following
+
+$(document).ready(function(){
+
+    
+    var id=2;
+    var i=1;
+    var body="";
+    var main_image="";
+    var title="";
+
+    //adding article images to preview and generating additional inputs
+    //also adding selectors to text area
+    $(document).on('change','.file_add',function(e){
+
+        $(this).after("<input name='file[]' type='file' id='file"+id+"' class='file_add form-control'/>");
+        $(this).attr('disabled',true);
+
+        $('#image_preview')
+        .append("<img width='200px' class='image_col' id='img"+i+"' height='200px' src='"+URL.createObjectURL(e.target.files[0])+"'>");
+        var tmpBody=$('#bodyArea').val()
+        
+
+        $('#bodyArea').val(tmpBody+" img"+i);
+
+
+
+        id++;
+        i++;
+        
+        console.log(e.target.files[0].name.length);
+    });
+    //clicking on images will remove them from preview div, textarea and will
+    //remove corresponding input
+    $(document).on('click','.image_col',function(){
+        var id="";
+        
+        id=$(this).attr('id');
+
+        
+        body= $('#bodyArea').val();
+        body=body.replace(id,'');
+        $('#bodyArea').val(body);
+
+        console.log(id);
+        id=id.substring(3);
+        console.log(id);
+
+
+        $(this).parent().siblings('.file_imgs').children('#file'+id).remove();
+        $(this).remove();
+
+        
+        });
+
+         // main image validation
+        if(document.getElementById("front_image").files.length == 0){
+            $('#front_image_error').html("Image not selected, please select image.");
+            $('#front_image_error').addClass('val_error');
+            $('#front_image').addClass('val_error');
+
+        }
+        $("#front_image").change(function(e){
+
+        if(document.getElementById("front_image").files.length == 0){
+    
+                
+            $('#front_image_error').html("Image not selected, please select image.");
+            $('#front_image_error').addClass('val_error').removeClass('val_success');
+            $('#front_image').addClass('val_error').removeClass('val_success');
+        }
+        else{
+
+            $('#front_image_error').html("File successfully added");
+            $('#front_image_error').addClass('val_success').removeClass('val_error');
+            $('#front_image').removeClass('val_error').addClass('val_success');
+            main_image=event.target.files;
+        }
+    
+        });
+
+        //Title validation
+        $("#title").on('focusout',function(){
+
+        var tmpTitle=$('#title').val();
+       
+        if(tmpTitle.length==0){
+            $('#title_error').html("Title is required.").addClass('val_error').removeClass('val_success');
+            $('#title').addClass('val_error').removeClass('val_success');
+        }else if(tmpTitle.length>30){
+            $('#title_error').html("Title must have 30 characters or less.").addClass('val_error').removeClass('val_success');
+            $('#title').addClass('val_error').removeClass('val_success');
+        }
+        else {
+
+            title=tmpTitle;
+            $('#title_error').html("");
+            $('#title').removeClass('val_error').addClass('val_success');
+        }
+        console.log(tmpTitle);
+        console.log(tmpTitle.length);
+        });
+
+        //taking value of text area on focus and validation
+        $("#bodyArea").on('focusout',function(){
+        body=$('#bodyArea').val();
+
+       if(body.length>0) 
+       {
+            $('#body_error').html('Text successfully added')
+            $('#body_error').removeClass('val_error').addClass('val_success').removeClass('val_allowed');
+        }
+        else {
+            $('#body_error').html('Text is optional');
+            $('#body_error').removeClass('val_error').removeClass('val_success').addClass('val_allowed');
+
+        }
+        });
+
+        //if everything is filled correctly
+   $('#submitArticle').on('click',function(e){
+
+    e.preventDefault();
+
+    if(title.length>0 
+     && title.length<=30 
+     && !jQuery.isEmptyObject(main_image) 
+     && body.length>0 
+     
+
+    ){
+      
+     $('#submitArticle').on('click' , function(e){
+
+         e.preventDefault();
+         var formData = new FormData($(this).parents('form')[0]);
+     
+         $.ajax({
+             url: 'http://localhost/project/api/create_article_api.php',
+             type: 'POST',
+             
+             xhr: function() {
+                 var myXhr = $.ajaxSettings.xhr();
+                 return myXhr;
+             },
+             success: function (data) {
+                 alert("Data Uploaded: "+data);
+             },
+             data: formData,
+             cache: false,
+             contentType: false,
+             processData: false
+         });
+         return false;
+     })
+               
+
+     }else {
+         $('#page_error').html('Something went wrong, please fill form correctly.');
+         $('#page_error').addClass('val_error');
+     }
+    
+
+});
+
+
+   
+});
+
--- main/create_article.php
+++ main/create_article.php
@@ -13,6 +13,7 @@ if(!$session->checkSession()) header('Location: articles.php');
 
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
@@ -36,33 +37,62 @@ require_once '../navigation/nav_bar.php';
 <div class="container">
 <div class="row">
 
-    <div class="col-md-2 col-sm-2"></div>
-    <div class="col-md-8 col-sm-8">
+    <div class="col-md-1 col-sm-1"></div>
+    <div class="col-md-10 col-sm-10">
     
     
-    <h1>Naslov naslov naslov</h1>
+    <h1>Create new Article:</h1>
     <hr>
-    <div  class="img-wrap">
-        <img class="img-fluid"  src="../assets/images/collection/home-office-336378_1920.jpg">
-    </div>
+    <div id="page_error">
+            
+        </div>
+    <form id="article-form" action="http://localhost/project/api/create_article_api.php" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="title">Title:</label>
+            <input type="text" name="title" class="form-control" id="title" placeholder="Enter article title:">
+        </div>
+        <div id="title_error">
+            
+        </div>
+    
+        <div class="form-group">
+            <label for="front_image">Main image:</label>
+            <input type="file" name="main_image" class="form-control" id="front_image" placeholder="Upload front image:">
+        </div>
+        <div id="front_image_error">
+            
+        </div>
 
-    <div class="text-wrap">
-        <hr>
-    <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+        <div class="form-group">
+        <label for="bodyArea">Text:</label>
+        <textarea class="form-control" name="body" id="bodyArea" rows="3"></textarea>
+        </div>
+        <div id="body_error">
+            
+        </div>
+
+        <div class="form-group file_imgs">
+            <label for="file0">Article images:</label>
+            <input name="file[]" type="file" id="file1"class='file_add form-control' />
+        </div>
+
+        <div id="image_collection_error">
+            
+        </div>
+        <div  id="image_preview">
+        
+        </div class="form-group">
+        <div >
+        <input type="submit" id="submitArticle" class="btn btn-success btn-block form-control" name='submitArticle' value="Submit Article"/>
+        </div>
+    </form>
+
     
-    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
-    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
-    
-    <p>by Username on 19.03.2009</p>
+
    
-    
-    </div>
 
     </div>
-    <div class="col-md-2 col-sm-2"></div>
+    <div class="col-md-1 col-sm-1"></div>
     
 
 </div>
@@ -72,9 +102,14 @@ require_once '../navigation/nav_bar.php';
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+<script type="text/javascript" src="../assets/js/upload_article_imgs.js"></script>
 </body>
 
 </html>
